590cc8bdb12c15029643d03a85b39be3b2d68640
atharva tamta <110754495+atharvatamta@users.noreply.github.com>
2026-02-21 15:05:25 +0000

    conversion of datatypes

---
+++ 01_basics/comparisonOfDT.js
@@ -0,0 +1,16 @@
+console.log(null > 0); // false
+//Comparison (>) converts null to a number before comparing.
+//null is converted to 0.
+// 0 > 0 is false.
+
+console.log(null == 0); // false
+// The equality check (==) does not convert null to a number.
+
+// null is only equal to undefined in loose equality (==), not to numbers.
+
+// So, null == 0 is false.
+
+ console.log(null >= 0); // true
+// The >= comparison also converts null to a number (0).
+
+// It becomes 0 >= 0, which is true.
